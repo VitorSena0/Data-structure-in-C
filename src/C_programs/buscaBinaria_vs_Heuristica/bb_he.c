a7ad2186534f7267d99e1146a7262e8277e13a8c
Vitor Leonardo <97699477+VitorSena0@users.noreply.github.com>
2024-08-13 12:27:57 -0300

    heurística e busca binária, tabelas hash

---
+++ src/C_programs/buscaBinaria_vs_Heuristica/bb_he.c
@@ -0,0 +1,212 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//-------------------------[CONSTANTES]-------------------------
+#define TAM_MAX_LINHA 200
+#define TAM_MAX_AUTOR 51
+#define TAM_MAX_TITULO 101
+
+//-------------------------[ESTRUTURAS]-------------------------
+typedef struct Livros {
+    unsigned long long ISBN;
+    char autor[TAM_MAX_AUTOR];
+    char titulo[TAM_MAX_TITULO];
+} Livros;
+
+//-------------------------[PROTÓTIPOS DE FUNÇÕES]-------------------------
+void processarDocumento(const char *arqInput, const char *arqOutput);
+int buscaBinaria(Livros *vetor, int fim, unsigned long long ISBN, int *iteracoesTotal, int *totalIteracoesBuscaAtual, int *indiceLivro);
+int buscaInterpolada(Livros *array, int n, unsigned long long ISBN, int *iteracoesTotal, int *totalIteracoesBuscaAtual);
+
+
+//-------------------------[FUNÇÃO MAIN]-------------------------
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Uso: %s <input_file> <output_file>\n", argv[0]);
+        return 1;
+    }
+
+    processarDocumento(argv[1], argv[2]);
+
+    return 0;
+}
+
+
+//-------------------------[FUNÇÃO DE PROCESSAMENTO DE DOCUMENTO]-------------------------
+void processarDocumento(const char *arqInput, const char *arqOutput) {
+    FILE *arqIn = fopen(arqInput, "r");
+    FILE *arqOut = fopen(arqOutput, "w");
+    if (!arqIn || !arqOut) {
+        printf("Erro ao abrir os arquivos\n");
+        if (arqIn) fclose(arqIn);
+        if (arqOut) fclose(arqOut);
+        return;
+    }
+
+    int qtdLivros;
+    if (fscanf(arqIn, "%d", &qtdLivros) != 1) { // Lê a quantidade de livros
+        printf("Erro ao ler a quantidade de livros\n");
+        fclose(arqIn);
+        fclose(arqOut);
+        return;
+    }
+
+    Livros *livros = (Livros *)malloc(qtdLivros * sizeof(Livros)); // Aloca memória para os livros
+    if (!livros) {
+        printf("Erro ao alocar memoria\n");
+        fclose(arqIn);
+        fclose(arqOut);
+        return;
+    }
+
+    char linha[TAM_MAX_LINHA]; // Armazenará as linha lidas do arquivo a cada iteração
+    char ISBN[14]; // Armazenará o ISBN lido do arquivo
+    int contaLivros = 0; // Contador de livros lidos
+
+    while (contaLivros < qtdLivros) { // Enquanto não percorrer todo o vetor de livros ele continua lendo os livros
+        if (fscanf(arqIn, "%13s", ISBN) != 1) { // Lê o ISBN
+            printf("Erro ao ler o ISBN\n");
+            free(livros);
+            fclose(arqIn);
+            fclose(arqOut);
+            return;
+        }
+        livros[contaLivros].ISBN = atoll(ISBN); // Converte o ISBN para inteiro
+        fgetc(arqIn); // Pula o espaço após o ISBN
+
+        if (fgets(linha, TAM_MAX_LINHA, arqIn) != NULL) { // Lê a linha do autor e título
+            char autor_e_titulo[TAM_MAX_LINHA]; // Armazenará o autor e título lido
+            sscanf(linha, "%[^\n]", autor_e_titulo); // Lê a linha até o \n
+
+            char *autor = strtok(autor_e_titulo, "&"); // Pega o autor até a aparicão do &
+            char *titulo = strtok(NULL, "\n"); // Pega o restante da linha
+
+            snprintf(livros[contaLivros].autor, TAM_MAX_AUTOR, "%s", autor);
+            snprintf(livros[contaLivros].titulo, TAM_MAX_TITULO, "%s", titulo);
+            contaLivros++; // Incrementa o contador de livros lidos
+        }
+    }
+
+    int qtdBusca; // Quantidade de buscas
+    if (fscanf(arqIn, "%d", &qtdBusca) != 1) { // Lê a quantidade de buscas
+        printf("Erro ao ler a quantidade de buscas\n");
+        free(livros);
+        fclose(arqIn);
+        fclose(arqOut);
+        return;
+    }
+    unsigned long long *ISBNs = (unsigned long long *)malloc(qtdBusca * sizeof(unsigned long long));  // Aloca memória para o vetor de ISBNs
+    if (!ISBNs) {
+        printf("Erro ao alocar memoria\n");
+        free(livros);
+        fclose(arqIn);
+        fclose(arqOut);
+        return;
+    }
+
+    fgetc(arqIn); // Pula o \n
+
+    for (int i = 0; i < qtdBusca; i++) {
+        if (fscanf(arqIn, "%13s", ISBN) != 1) {
+            printf("Erro ao ler o ISBN de busca\n");
+            free(ISBNs);
+            free(livros);
+            fclose(arqIn);
+            fclose(arqOut);
+            return;
+        }
+        ISBNs[i] = atoll(ISBN);
+    }
+
+    //----------Inicialização de variáveis para contagem de iterações e comparação entre as buscas----------
+    int venceuInterpo = 0;
+    int venceuBinary = 0;
+    int totalIteracoesBin = 0;
+    int totalIteracoesInter = 0;
+    int contaIteracoesAtualBuscaBin;
+    int contaIteracoesAtualBuscaInter;
+    int indiceLivro;
+
+    //----------Busca dos livros----------
+    for (int i = 0; i < qtdBusca; i++) {
+
+        int numIteracoesBuscaAtualB = buscaBinaria(livros, contaLivros, ISBNs[i], &totalIteracoesBin, &contaIteracoesAtualBuscaBin, &indiceLivro); // Busca binária
+        int numIteracoesBuscaAtualI = buscaInterpolada(livros, qtdLivros, ISBNs[i], &totalIteracoesInter, &contaIteracoesAtualBuscaInter); // Busca interpolada
+
+        if (numIteracoesBuscaAtualB == -1 || numIteracoesBuscaAtualI == -1) {
+            fprintf(arqOut, "[%llu]B=%d,I=%d:%s\n", ISBNs[i], contaIteracoesAtualBuscaBin, contaIteracoesAtualBuscaInter, "ISBN_NOT_FOUND");
+        } else {
+            fprintf(arqOut, "[%llu]B=%d,I=%d:Author:%s,Title:%s\n", ISBNs[i], numIteracoesBuscaAtualB, numIteracoesBuscaAtualI, livros[indiceLivro].autor, livros[indiceLivro].titulo);
+        }
+        if (contaIteracoesAtualBuscaInter <= contaIteracoesAtualBuscaBin) {
+            venceuInterpo++;
+        } else {
+            venceuBinary++;
+        }
+    }
+
+    //----------Cálculo da média de iterações----------
+    int mediaIteracoesBinaria = totalIteracoesBin / qtdBusca;
+    int mediaIteracoesInterpolada = totalIteracoesInter / qtdBusca;
+
+    fprintf(arqOut, "BINARY=%d:%d\n", venceuBinary, mediaIteracoesBinaria);
+    fprintf(arqOut, "INTERPOLATION=%d:%d\n", venceuInterpo, mediaIteracoesInterpolada);
+
+    free(ISBNs);
+    free(livros);
+    fclose(arqIn);
+    fclose(arqOut);
+}
+
+
+
+//-------------------------[FUNÇÕES DE BUSCA]-------------------------
+int buscaBinaria(Livros *vetorLivros, int tamanhoVetor, unsigned long long ISBN, int *iteracoesTotal, int *totalIteracoesBuscaAtual, int *indiceLivro) {
+    *indiceLivro = 0;
+    int inicio = 0;
+    int fim = tamanhoVetor-1;
+    *totalIteracoesBuscaAtual = 0;
+    while (inicio <= fim) {
+        
+        (*iteracoesTotal)++;
+        (*totalIteracoesBuscaAtual)++;
+        int meio = (inicio + fim) / 2;
+        if (vetorLivros[meio].ISBN == ISBN){
+
+            *indiceLivro = meio;
+         return *totalIteracoesBuscaAtual;
+
+        }
+        if (vetorLivros[meio].ISBN < ISBN) inicio = meio + 1;
+        else fim = meio - 1;
+    }
+    (*totalIteracoesBuscaAtual)++;
+    *indiceLivro = inicio;
+    return -1;
+}
+
+
+//-------------------------[FUNÇÕES DE BUSCA]-------------------------
+int buscaInterpolada(Livros *vetorLivros, int tamanhoVetor, unsigned long long ISBN, int *iteracoesTotal, int *totalIteracoesBuscaAtual) {
+    int inicio = 0, fim = tamanhoVetor - 1;
+    *totalIteracoesBuscaAtual = 0;
+    while (inicio <= fim) {
+        (*iteracoesTotal)++;
+        (*totalIteracoesBuscaAtual)++;
+        if (vetorLivros[fim].ISBN == vetorLivros[inicio].ISBN) {
+            if (vetorLivros[inicio].ISBN == ISBN) return *totalIteracoesBuscaAtual;
+            return -1;
+        }
+
+        int pos = inicio + ((vetorLivros[fim].ISBN - vetorLivros[inicio].ISBN)) % (fim - inicio + 1);
+
+        if (pos < inicio || pos > fim) break;  // Verifica se o índice está fora dos limites
+
+        if (vetorLivros[pos].ISBN == ISBN) return *totalIteracoesBuscaAtual;
+
+        if (vetorLivros[pos].ISBN < ISBN) inicio = pos + 1;
+        else fim = pos - 1;
+    }
+    return -1;
+}
--- src/C_programs/hash/hashBusca.c
+++ src/C_programs/hash/hashBusca.c
@@ -2,24 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <sys/types.h>
 
 #define TAMANHO_MAX_PALAVRA 101
 
 typedef struct Servidor {
-    uint16_t qtdServidores;
     uint16_t qtdMaxRequisicoes;
     uint16_t numeroAtualRequisicoes;
-    char (*requisicoes)[TAMANHO_MAX_PALAVRA];
+    char **palavrasLinha; // Ponteiro para a lista de palavrasLinha
 } Servidor;
 
 void processarDocumentos(char *input, char *output);
 void checksum(char *string, uint32_t *somaBitaBit, uint16_t *i);
-Servidor *criaServidor(uint16_t qtdServidores, uint32_t qtdMaxRequisicoes);
+Servidor *criaServidor(uint16_t qtdMaxRequisicoes);
 uint32_t hash1(uint32_t checa_soma, uint32_t tamanhoTabelaHash);
-uint32_t hash2(uint32_t checa_soma, uint32_t tamanhoTabelaHash);
-// uint8_t ehPrimo(uint32_t n);
-// uint32_t proximo_primo(uint32_t n);
-
+uint32_t doubleHash(uint32_t checa_soma, uint16_t tentativa, uint32_t tamanhoTabelaHash);
+void imprimirFilaServidor(Servidor *servidor, uint32_t id, FILE *arqOut);
+void liberaServidores(Servidor *servidores, uint16_t qtdServidores);
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -36,152 +35,183 @@ void processarDocumentos(char *input, char *output) {
     FILE *arqInput = fopen(input, "r");
     FILE *arqOutput = fopen(output, "w");
     if (!arqInput || !arqOutput) {
-        printf("Erro ao abrir os arquivos");
+        printf("Erro ao abrir os arquivos\n");
         if (arqInput) fclose(arqInput);
         if (arqOutput) fclose(arqOutput);
         return;
     }
 
-    uint16_t qtdServidores, qtdMaxRequisicoes, reqPorLinha;
+    uint16_t qtdServidores, qtdMaxRequisicoes, palavrasPorLinha;
     uint32_t numTotalRequisicoes;
     char *linha = NULL;
     size_t tamanhoLinha = 0;
-    uint32_t contaCaracteresLinha;
 
     if (fscanf(arqInput, "%hu %hu", &qtdServidores, &qtdMaxRequisicoes) != 2) {
         printf("Erro ao ler qtdServidores e qtdMaxRequisicoes\n");
         fclose(arqInput);
         fclose(arqOutput);
-        free(linha);
         return;
     }
 
-    if (fscanf(arqInput, "%hu", &numTotalRequisicoes) != 1) {
+    if (fscanf(arqInput, "%u", &numTotalRequisicoes) != 1) {
         printf("Erro ao ler numTotalRequisicoes\n");
         fclose(arqInput);
         fclose(arqOutput);
-        free(linha);
         return;
     }
 
     Servidor *servidores = malloc(qtdServidores * sizeof(Servidor));
-        if (servidores == NULL) {
+    if (servidores == NULL) {
         printf("Erro ao alocar servidores\n");
         fclose(arqInput);
         fclose(arqOutput);
-        free(linha);
         return;
     }
 
     for (uint16_t i = 0; i < qtdServidores; i++) {
-        servidores[i] = *criaServidor(qtdServidores, qtdMaxRequisicoes);
-        if (servidores[i].requisicoes == NULL) {
-            printf("Erro ao alocar memoria para as requisicoes\n");
+        servidores[i] = *criaServidor(qtdMaxRequisicoes);
+        if (servidores[i].palavrasLinha == NULL) {
+            printf("Erro ao alocar palavrasLinha\n");
+            liberaServidores(servidores, i); // Libera memória alocada até o momento
             fclose(arqInput);
             fclose(arqOutput);
-            free(linha);
-            free(servidores);
             return;
         }
     }
 
-    ssize_t leitura;// Variável para armazenar o retorno de getline
-
+    ssize_t leitura;
     while ((leitura = getline(&linha, &tamanhoLinha, arqInput)) != -1) {
-    uint32_t somaBitaBit = 0; // Representa o checksum da linha atual
+        uint32_t somaBitaBit = 0;
         size_t length = strcspn(linha, "\n");
         if (linha[length] == '\n') {
             linha[length] = '\0';
         }
 
-        char *token = strtok(linha, " "); // Divida a linha em palavras usando strtok
+        char *token = strtok(linha, " ");
         if (token != NULL) {
-            reqPorLinha = atoi(token);
-            token = strtok(NULL, " "); // Ignorar a primeira palavra
+            palavrasPorLinha = atoi(token);
+            token = strtok(NULL, " ");
         }
-        if(token == NULL){ // Pula para a próxima linha a primeira linha que não tem palavras e pula a ultima linha que não tem palavras
+        if (token == NULL) { // Se a linha estiver vazia
             continue;
         }
-        uint16_t sal = 1; // Sal é o número da posição da letra na palavra, serve para que caso haja palavras com os mesmo simbolos mas em posições diferentes, o checksum seja diferente
+
+        char palavras[palavrasPorLinha][TAMANHO_MAX_PALAVRA];
+        uint16_t sal = 1;
+        uint16_t contaPalavras = 0;
         while (token != NULL) {
-            // Faça alguma operação com cada palavra da mesma linha
             checksum(token, &somaBitaBit, &sal);
-            // Obtenha o próximo token
+            strcpy(palavras[contaPalavras], token);
+            contaPalavras++;
             token = strtok(NULL, " ");
         }
 
         uint32_t valorHash1 = hash1(somaBitaBit, qtdServidores);
+        uint32_t posicao = valorHash1;
+        uint16_t tentativa = 0;
 
-    
+        while (servidores[posicao].numeroAtualRequisicoes >= servidores[posicao].qtdMaxRequisicoes) {
+            posicao = doubleHash(somaBitaBit, tentativa, qtdServidores);
+            tentativa++;
+            if (tentativa >= qtdServidores) { // Evitar loop infinito
+                for(uint16_t i = 0; i < qtdServidores; i++) {
+                    if (servidores[i].numeroAtualRequisicoes < servidores[i].qtdMaxRequisicoes) {
+                        posicao = i;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+
+        Servidor *servidor = &servidores[posicao];
+
+        servidor->palavrasLinha[servidor->numeroAtualRequisicoes] = malloc(TAMANHO_MAX_PALAVRA * palavrasPorLinha);
+        if (servidor->palavrasLinha[servidor->numeroAtualRequisicoes] == NULL) {
+            printf("Erro ao alocar memória para palavrasLinha\n");
+            free(linha);
+            liberaServidores(servidores, qtdServidores); // Libera memória de todos os servidores
+            fclose(arqInput);
+            fclose(arqOutput);
+            return;
+        }
+
+        servidor->palavrasLinha[servidor->numeroAtualRequisicoes][0] = '\0'; // Inicializa a string como vazia
+
+        for (uint16_t i = 0; i < palavrasPorLinha; i++) {
+            strcat(servidor->palavrasLinha[servidor->numeroAtualRequisicoes], palavras[i]);
+            if (i < palavrasPorLinha - 1) {
+                strcat(servidor->palavrasLinha[servidor->numeroAtualRequisicoes], " ");
+            }
+        }
+
+        if (tentativa > 0) {
+            fprintf(arqOutput, "S%u->S%u\n", valorHash1, posicao);
+        }
+        fprintf(arqOutput, "[S%u] ", posicao);
+
+        servidor->numeroAtualRequisicoes++;
+
+        // Imprimir a fila de requisições do servidor
+        imprimirFilaServidor(servidor, posicao, arqOutput);
     }
 
-    free(linha); // Liberar a memória alocada por getline
+    // Liberação da memória
+    free(linha);
+    liberaServidores(servidores, qtdServidores);
     fclose(arqInput);
     fclose(arqOutput);
 }
 
-Servidor *criaServidor(uint16_t qtdServidores, uint32_t qtdMaxRequisicoes) {
+Servidor *criaServidor(uint16_t qtdMaxRequisicoes) {
     Servidor *servidor = malloc(sizeof(Servidor));
     if (servidor != NULL) {
-        servidor->qtdServidores = qtdServidores;
         servidor->qtdMaxRequisicoes = qtdMaxRequisicoes;
         servidor->numeroAtualRequisicoes = 0;
-        servidor->requisicoes = malloc(qtdMaxRequisicoes * sizeof(char[TAMANHO_MAX_PALAVRA]));
-        if (servidor->requisicoes == NULL) {
-            printf("Erro ao alocar memoria para as requisicoes\n");
+        servidor->palavrasLinha = malloc(sizeof(char *) * qtdMaxRequisicoes);
+        if (servidor->palavrasLinha == NULL) {
             free(servidor);
             return NULL;
+        }
+        for (uint16_t i = 0; i < qtdMaxRequisicoes; i++) {
+            servidor->palavrasLinha[i] = NULL;
         }
     }
     return servidor;
 }
 
-uint8_t ehPrimo(uint32_t n) {
-    if (n <= 1) return 0;
-    if (n <= 3) return 1;
-    if (n % 2 == 0 || n % 3 == 0) return 0;
-
-    for (uint32_t i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) return 0;
+void checksum(char *string, uint32_t *somaBitaBit, uint16_t *i) {
+    while (*string != '\0') {
+        *somaBitaBit ^= (uint32_t)((*string));
+        string++;
+        (*i)++;
     }
-    return 1;
 }
 
-// uint32_t proximo_primo(uint32_t n) {
-//     if (n <= 1) return 2;
-//     if (n == 2) return 3;
-
-//     uint32_t primo = n;
-//     if (primo % 2 == 0) {
-//         primo++;
-//     } else {
-//         primo += 2;
-//     }
-
-//     while (!ehPrimo(primo)) {
-//         primo += 2;
-//     }
-
-//     return primo;
-// }
-
-void checksum(char *string, uint32_t *somaBitaBit,uint16_t *i){
-  while(*string != '\0'){
-    *somaBitaBit ^= (uint32_t)((*string)*(*i));
-    string++;
-    (*i)++;
-  };
-}
-
-// Função de hashing H1
 uint32_t hash1(uint32_t checa_soma, uint32_t tamanhoTabelaHash) {
     return (7919 * checa_soma) % tamanhoTabelaHash;
 }
 
-// Função de hashing H2
-uint32_t hash2(uint32_t checa_soma, uint32_t tamanhoTabelaHash) {
-    return (104729 * checa_soma + 123) % tamanhoTabelaHash;
+uint32_t doubleHash(uint32_t checa_soma, uint16_t tentativa, uint32_t tamanhoTabelaHash) {
+    return ((7919 * checa_soma) + (tentativa * (104729 * checa_soma + 123))) % tamanhoTabelaHash; // Garantir que hash2 seja ímpar
 }
 
+void imprimirFilaServidor(Servidor *servidor, uint32_t id, FILE *arqOut) {
+    for (uint16_t i = 0; i < servidor->numeroAtualRequisicoes; i++) {
+        if (i == servidor->numeroAtualRequisicoes - 1) {
+            fprintf(arqOut, "%s\n", servidor->palavrasLinha[i]);
+        } else {
+            fprintf(arqOut, "%s, ", servidor->palavrasLinha[i]);
+        }
+    }
+}
 
-
+void liberaServidores(Servidor *servidores, uint16_t qtdServidores) {
+    for (uint16_t i = 0; i < qtdServidores; i++) {
+        for (uint16_t j = 0; j < servidores[i].numeroAtualRequisicoes; j++) {
+            free(servidores[i].palavrasLinha[j]);
+        }
+        free(servidores[i].palavrasLinha);
+    }
+    free(servidores);
+}
